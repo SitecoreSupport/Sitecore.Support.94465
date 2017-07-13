d34fe305fe39195ec77f4f71bf93c9bade0bcbe3
Anton Kechashin <atk@sitecore.net>
2017-07-13 14:25:51 +0300

    Initial commit.

---
+++ src/Sitecore.Support.94465/Pipelines/RenderField/ProtectedImageLinkRenderer.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.Pipelines.RenderField
+{
+    using Sitecore.Pipelines.RenderField;
+
+    public class ProtectedImageLinkRenderer : Sitecore.Pipelines.RenderField.ProtectedImageLinkRenderer
+    {
+        public new void Process(RenderFieldArgs args)
+        {
+            if (Sitecore.Context.PageMode.IsExperienceEditor) // don't add hash in Experience Editor
+            {
+                return;
+            }
+            base.Process(args);
+        }
+    }
+}
